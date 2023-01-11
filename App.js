fe4aa178f126e4308d5b645e911780e9deb5f735
tgthiag <tgthiag@gmail.com>
2023-01-10 23:42:26 -0300

    Finnishing report

--- App.js
+++ App.js
@@ -1,42 +1,42 @@
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Checklist from "./src/components/screens/flap";
 import MainPage from "./src/components/screens/main";
 import Report from "./src/components/screens/report";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
-// const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={"main"}>
-        <Stack.Screen
-          name="main"
-          component={MainPage}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="checklist"
-          component={Checklist}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="report"
-          component={Report}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-      {/* <Toast position={"bottom"} bottomOffset={90} /> */}
-      {/* SIDEBAR WILL BE ADDED IN THE FUTURE */}
-      {/* <Drawer.Navigator>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={"main"}>
+          <Stack.Screen
+            name="main"
+            component={MainPage}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="checklist"
+            component={Checklist}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="report"
+            component={Report}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+        {/* <Toast position={"bottom"} bottomOffset={90} /> */}
+        {/* SIDEBAR WILL BE ADDED IN THE FUTURE */}
+        {/* <Drawer.Navigator>
         <Drawer.Screen name="Home" component={MainPage}  />
         <Drawer.Screen name="Conference" component={Checklist} />
       </Drawer.Navigator> */}
-    </NavigationContainer>
+        {/* </AppContext.Provider> */}
+      </NavigationContainer>
   );
 }
 
