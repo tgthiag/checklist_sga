f4798dd433ec1595e9d3cd1dcb52272d7bf7ccab
tgthiag <tgthiag@gmail.com>
2023-02-22 03:18:01 -0300

    Get Current Date

--- src/components/functions/getDate.js
+++ src/components/functions/getDate.js
@@ -1,27 +1,8 @@
 export const getCurrentDate = () => {
-  var dt = changeTimeZone(new Date,'HST')
-  var date = dt.getDate();
-  var month = dt.getMonth() + 1;
-  var year = dt.getFullYear();
+  let dt = new Date()
+  let date = dt.getDate();
+  let month = dt.getMonth() + 1;
+  let year = dt.getFullYear();
 
-  //Alert.alert(date + '-' + month + '-' + year);
-  // You can turn it in to your desired format
-console.log('timezone' ,changeTimeZone(new Date, 'HST'))
-  return year + "-" + month + "-" + date; //format: d-m-y;
+  return year + "-" + month + "-" + date;
 };
-
-function changeTimeZone(date, timeZone) {
-  if (typeof date === 'string') {
-    return new Date(
-      new Date(date).toLocaleString('en-US', {
-        timeZone,
-      }),
-    );
-  }
-
-  return new Date(
-    date.toLocaleString('en-US', {
-      timeZone,
-    }),
-  );
-}
